c5ac40cbc1277147b003dfd9b72508fc0ab42d34
Patrick Basquel <pbasquel@apple.com>
2019-11-16 13:26:21 -0800

    + Tested and validated, simulation complete.
    + Added TrafficObject class
    + Built out waitForGreen, cycleThroughPhases and simulate functions
    + Built out random number generator for traffic lights
    + Defined MessageQueue class
    + Implemented send & receive functions.
    ! Fixed Segmentation Fault 11 seen, when condition variable was calling notify_one() twice.

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -1,58 +1,91 @@
 #include <iostream>
+#include <chrono>
 #include <random>
+#include <cmath>
 #include "TrafficLight.h"
 
-/* Implementation of class "MessageQueue" */
+double getRandomNumber(double min, double max);
 
-/* 
 template <typename T>
 T MessageQueue<T>::receive()
 {
-    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
-    // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
+    std::unique_lock<std::mutex> rLock(_mtx);
+    _condvar.wait(rLock);
+    T msg = std::move(_queue.front());
+    _queue.pop_front();
+    return msg;
 }
 
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
-    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
-    // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> sLock(_mtx);
+    _queue.push_back(msg);
+    _condvar.notify_one();    
 }
-*/
 
-/* Implementation of class "TrafficLight" */
-
-/* 
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
 }
 
-void TrafficLight::waitForGreen()
+TrafficLight::~TrafficLight()
 {
-    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
-    // runs and repeatedly calls the receive function on the message queue. 
-    // Once it receives TrafficLightPhase::green, the method returns.
+    _currentPhase = red;
 }
 
-TrafficLightPhase TrafficLight::getCurrentPhase()
+void TrafficLight::waitForGreen()
+{
+    TrafficLightPhase vPhase;
+    while ( true ) {
+        vPhase = trafficQue.receive();
+        if (vPhase == green ){
+            return ;
+        }
+    }
+}
+
+TrafficLight::TrafficLightPhase TrafficLight::getCurrentPhase()
 {
     return _currentPhase;
 }
 
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    std::thread t(&TrafficLight::cycleThroughPhases, this); 
+    threads.push_back(std::move(t));
 }
 
-// virtual function which is executed in a thread
 void TrafficLight::cycleThroughPhases()
 {
-    // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
-    // and toggles the current phase of the traffic light between red and green and sends an update method 
-    // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+    while (true) {
+        std::chrono::milliseconds runTime (0);
+        auto startTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()); // time now in milliseconds 
+        auto lightTimer = getRandomNumber(4.0, 6.0) * 1000; // milliseconds
+        while ( runTime.count() <  lightTimer ) {
+            runTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()) - startTime;
+        }
+        _currentPhase = ( _currentPhase == red ) ? green : red; 
+        TrafficLightPhase light = _currentPhase;       
+        trafficQue.send(std::move(light));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
 }
 
-*/
+double getRandomNumber(double min, double max )
+{
+    // standard method of random number generator
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<double> dist( min, max);
+    return dist(mt);
+}
+
+long decimalValue(double num)
+{
+    int factor = 100;
+    int decimal =  (int) ( num * factor ) %factor ;
+    double value = std::round(num);
+    long dec = decimal / factor ;
+    return (value + dec );
+}
